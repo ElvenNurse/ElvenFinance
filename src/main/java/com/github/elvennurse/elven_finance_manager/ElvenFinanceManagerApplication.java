73a524e0626c58af0a8064e636d528bfa2b3468f
Andrii Voloshyn <voland92@ukr.net>
2024-11-13 10:04:32 +0200

    Initialize Spring Boot project with Maven wrapper
    This commit sets up the Elven Finance Manager project using Spring Boot and Maven.

---
+++ src/main/java/com/github/elvennurse/elven_finance_manager/ElvenFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.elvennurse.elven_finance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElvenFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElvenFinanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/elvennurse/elven_finance_manager/ElvenFinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elvennurse.elven_finance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElvenFinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
